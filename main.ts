c78c35910de5d18c3652c00b46b9d1bbb10cb183
Mirae.JK <158263388+javeria-kamran@users.noreply.github.com>
2024-05-10 12:49:17 +0500

    wordcount

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ let words = users.paragraph.trim().split(" ")
 console.log("Yuor word count is " + " " + words.length + "\n");
 let will=await inquirer.prompt({
     type:"list",
-    name:"Want To",
+    name:"WantTo",
     message:"Do you want to know letter count?",
     choices:["Yes","No"]
 })
